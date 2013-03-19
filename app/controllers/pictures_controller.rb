082ae0e3ef248abe52d72ad5c7b01563accda254
Bitmaker Labs <bitmakerlabs@Bitmaker11.local>
2013-03-19 15:52:10 -0400

    transferred CSS out of template file

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,9 +4,20 @@ class PicturesController < ApplicationController
 
 	def index
 		@greeting = "Ma Dawgs"
-		@pictures = ['http://serpentorslair.com/wp-content/uploads/2013/02/megan-fox-wallpaper-16.jpg',
-			'http://gallery.rennlist.com/albums/album441/bitch.gif',
-			'http://i.imgur.com/Hebp5.jpg']	
+		@pictures = [
+			{
+				:title => "Megan Fox",
+				:artist => "Samir",
+				:url => 'http://serpentorslair.com/wp-content/uploads/2013/02/megan-fox-wallpaper-16.jpg'},
+			{
+				:title => "Quit Bitchin",
+				:artist => "Unknown",
+				:url => 'http://gallery.rennlist.com/albums/album441/bitch.gif'},
+			{
+				:title => "Jenga",
+				:artist => "Unknown",
+				:url => 'http://i.imgur.com/Hebp5.jpg'}
+		]	
 	end
 
 
