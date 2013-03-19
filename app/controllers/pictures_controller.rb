1a56dfa3a748e4f4997919b63366e70935fc8d46
Bitmaker Labs <bitmakerlabs@Bitmaker11.local>
2013-03-19 15:23:17 -0400

    Added Megan Fox, formatted page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,8 +3,8 @@
 class PicturesController < ApplicationController
 
 	def index
-		@greeting = "Ma Dawg"
-		@pictures = ['http://notfunny.co/wp-content/uploads/2012/11/ElwoodUglyDog-funny-ugly-dogs.jpg',
+		@greeting = "Ma Dawgs"
+		@pictures = ['http://serpentorslair.com/wp-content/uploads/2013/02/megan-fox-wallpaper-16.jpg',
 			'http://gallery.rennlist.com/albums/album441/bitch.gif',
 			'http://i.imgur.com/Hebp5.jpg']	
 	end
