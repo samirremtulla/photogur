fb1d43bc692173a382d9f443bab9c6dfaffc09c2
Bitmaker Labs <bitmakerlabs@Bitmaker11.local>
2013-03-19 14:49:03 -0400

    added picturesgit status

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,12 @@
 # pictures_controller.rb
 
-class PicturesController < ActionController::Base
+class PicturesController < ApplicationController
 
 	def index
-
-	
+		@greeting = "Ma Dawg"
+		@pictures = ['http://notfunny.co/wp-content/uploads/2012/11/ElwoodUglyDog-funny-ugly-dogs.jpg',
+			'http://gallery.rennlist.com/albums/album441/bitch.gif',
+			'http://i.imgur.com/Hebp5.jpg']	
 	end
 
 
