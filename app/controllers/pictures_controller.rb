bf39d68fa39b73dc18ab7c5c100a78c2520640d6
Bitmaker Labs <bitmaker@Bitmaker09.local>
2013-03-20 10:58:09 -0400

    add database functionality

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,11 +4,12 @@ class PicturesController < ApplicationController
 
 	before_filter :load_pictures
 
-	#def index
-	#end
+	def index
+	end
 
 
 	def show
+		# id comes from the route
 		@picture = @pictures[params[:id].to_i]
 
 	end
@@ -17,25 +18,41 @@ class PicturesController < ApplicationController
 	end
 
 	def create
+		
+		@picture = Picture.new
+		@picture.url = params[:url]
+		@picture.title = params[:title]
+		@picture.artist = params[:artist]
+		success = @picture.save
+		
+		if success
+			redirect_to pictures_path 
+		end
+
 		# render :text => "Saving a picture. URL: #{params[:url]} Artist: #{params[:artist]} Title: #{params[:title]}" 
+	
 	end
 
 
 	def load_pictures
-		@pictures = [
-			{
-				:title => "Megan Fox",
-				:artist => "Samir",
-				:url => 'http://serpentorslair.com/wp-content/uploads/2013/02/megan-fox-wallpaper-16.jpg'},
-			{
-				:title => "Quit Bitchin",
-				:artist => "Unknown",
-				:url => 'http://gallery.rennlist.com/albums/album441/bitch.gif'},
-			{
-				:title => "Jenga",
-				:artist => "Unknown",
-				:url => 'http://i.imgur.com/Hebp5.jpg'}
-		]	
+
+		@pictures = Picture.all
+
+
+		# @pictures = [
+		# 	{
+		# 		:title => "Megan Fox",
+		# 		:artist => "Samir",
+		# 		:url => 'http://serpentorslair.com/wp-content/uploads/2013/02/megan-fox-wallpaper-16.jpg'},
+		# 	{
+		# 		:title => "Quit Bitchin",
+		# 		:artist => "Unknown",
+		# 		:url => 'http://gallery.rennlist.com/albums/album441/bitch.gif'},
+		# 	{
+		# 		:title => "Jenga",
+		# 		:artist => "Unknown",
+		# 		:url => 'http://i.imgur.com/Hebp5.jpg'}
+		# ]	
 	end
 
 
