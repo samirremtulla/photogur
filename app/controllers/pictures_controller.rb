88300b1c127425b1ddcc2d90036e517a846a9aeb
Samir Remtulla <samir.remtulla@gmail.com>
2013-03-25 10:12:47 -0400

    Added delete functionaltiy, removed public/index.html

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -58,6 +58,15 @@ class PicturesController < ApplicationController
 		end	
 	end
 
+	def destroy
+		@picture = Picture.find(params[:id])
+		@picture.destroy
+		redirect_to pictures_path
+	end
+
+
+
+
 	# def load_pictures
 
 
